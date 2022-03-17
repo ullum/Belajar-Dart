9c41ea21d50abbf7fa3af891696ec393c576d5af
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2022-03-17 09:13:46 +0700

    operator

---
+++ operator.dart
@@ -0,0 +1,50 @@
+void main() {
+  var a = 10;
+  var b = 4;
+  var c = a + b;
+
+  /* istilah 
+  operands -> representasi dari data (a & b)
+  operators -> sesuatu yang memutuskan operands diproses [+, -, /, *]
+  */
+
+  // Aritmatic Operator
+  // penjumlahan
+  var penjumlahan = a + b;
+  // pengurangan
+  var pengurangan = a - b;
+  // perkalian
+  var perkalian = a * b;
+  // pembagian
+  var pembagian = a / b;
+  // modulo
+  var sisa = a % b;
+
+  print(penjumlahan);
+  print(pengurangan);
+  print(perkalian);
+  print(pembagian);
+  print(sisa);
+
+  // Equality & Relational Operator
+  print(a < b);
+  print(a > b);
+  print(a == b);
+  print(a != b);
+  print(a >= b);
+  print(a <= b);
+
+  // Logical Operator
+  print('Logical Operator');
+  bool x = true;
+  bool y = false;
+
+  // && AND -> bernilai false apabila salah satu ebrnilai false
+  print(x && y);
+
+  // || OR -> bernilai true apabila   salah satu bernilai true
+  print(x || y);
+
+  // ! NOT -> nilai menjadi berlawanan
+  print(!x);
+}
