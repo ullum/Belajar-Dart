9bd2404a6a695da098ff0e69b95da74ce21292fe
Moh. Bahrul Ulum <48970016+ullum@users.noreply.github.com>
2022-06-25 19:29:14 +0700

    Challenges 5

---
+++ Class_And_Object/challenge5.dart
@@ -0,0 +1,14 @@
+void mian() {
+  /*
+    Challenge 5 
+    1. Buatlah class E-Wallet dengan ketentuan sebagai berikut:
+      # Properties:
+        - namaPemilik
+        - saldo
+        - mutasi
+      # Methods:
+        - Getter and Setter untuk setiap Properties
+        - Transfer() // dari rekening pemilik
+        - request() // ke rekening pemilik
+   */
+}
